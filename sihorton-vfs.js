275a7be81fae11b1bc2345be8eeed014e5c5dc74
Simon Horton <sij.horton@gmail.com>
2012-11-29 10:34:28 +0100

    created initial signature for the virtual file system.

---
+++ sihorton-vfs.js
@@ -0,0 +1,30 @@
+var fs = require('fs')
+	path = require('path');
+
+/**
+* A virtual file system served from a file on disk.
+*/
+var vfs = function(path) {
+	var Me = {
+	
+	}
+	return Me;
+}
+/**
+* Wrapper for a real directory on disk.
+*/
+var fs-wrapper = function(path) {
+	var Me = {
+	
+	}
+	return Me;
+}
+/**
+* mount a file system from a particular path
+* if directory provide an fs wrapper, if file open
+* and provide vfs object.
+*/
+module.exports.Mount = function(path) {
+	
+	return vfs(path);
+}
