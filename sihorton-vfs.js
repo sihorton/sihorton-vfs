46d92e44b8d27e196bc2f7687ac3acaa2e6b59bc
Simon Horton <sij.horton@gmail.com>
2012-11-29 20:58:55 +0100

    implemented better open and close.

--- sihorton-vfs.js
+++ sihorton-vfs.js
@@ -461,14 +461,19 @@ var appfs = function(mountpath, stats, readyCall) {
 		//poor initial implementation
 		,open(fpath,flags,mode,done) {
 			var fd = Me.fds.length;
-			Me.fds.push(fpath);
+			Me.fds.push({
+				path:fpath
+				,state:'open'
+			});
 			if (done) {
 				done(null,fd);
 			}
 		},close(fd) {
-			//@ToDo mark this as closed and remove it somehow.
+			Me.fds[fd].state = 'closed';
+		}/*,write(fd, buffer, offset, length, position, callback) {
+			fpath = Me.fds[fd];
+			var write1 = Me.createWriteStream(fpath);
 		}
-		//write(fd, buffer, offset, length, position, [callback])#
 		fs.writeSync(fd, buffer, offset, length, position)
 		fs.read(fd, buffer, offset, length, position, [callback])
 		fs.readSync(fd, buffer, offset, length, position)
